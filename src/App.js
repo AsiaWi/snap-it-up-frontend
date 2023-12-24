110b4a383d1085fe48df02df48cf999160e28836
AsiaWi <joannawitek@icloud.com>
2023-12-24 19:36:02 +0000

    Add fontawesome, google fonts, favicon and logo to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <Button variant="primary">Primary</Button>
+      <NavBar/>
     </div>
   );
 }
