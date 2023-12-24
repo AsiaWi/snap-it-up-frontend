a82fb859a8380769184877f8eedfbd3d092a221c
AsiaWi <joannawitek@icloud.com>
2023-12-24 16:38:03 +0000

    Add bootstrap and test-working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
