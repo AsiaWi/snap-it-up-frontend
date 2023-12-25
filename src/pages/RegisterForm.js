620e852b1a9db97dc56b2d096a81cc19dfb791ee
AsiaWi <joannawitek@icloud.com>
2023-12-25 18:33:36 +0000

    Add basic form structure for registration page, fix responsiveness for the page

--- src/pages/RegisterForm.js
+++ src/pages/RegisterForm.js
@@ -1,59 +1,102 @@
 // css imports
-import formStyling from '../styles/SignInAndRegisterForms.module.css'
+import formStyling from "../styles/SignInAndRegisterForms.module.css";
+import btnStyling from "../styles/Button.module.css";
 // react imports
-import React from 'react'
-import {Container, Row, Col, Form, Button, Image} from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Form, Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
   return (
-    <Container className="mt-3">
-      <Row className="d-none d-md-flex">
-          <Col className="text-center">
-              <Image
-                 className={formStyling.Image}
-                src={"https://res.cloudinary.com/dmod5eglu/image/upload/v1703462762/form_img_bl8th5.png"}/>
-        </Col>
-
-        <Col md={6} sm={10}>
+    <Container>
+      <Row className="p-2">
+        <Col
+          xs={12}
+          md={6}
+          lg={6}
+          xl={6}
+          className={`mb-4 mb-md-0 ${formStyling.Form}`}
+        >
+          <h1 className={formStyling.header}>REGISTER</h1>
           <Form>
-            <Form.Group controlId="">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="" placeholder="" />
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={formStyling.FormInput}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
             </Form.Group>
 
-            <Form.Group controlId="">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={formStyling.FormInput}
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
             </Form.Group>
 
-
-            <Button  type="submit">
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control
+                className={formStyling.FormInput}
+                type="password"
+                placeholder="Confirm password"
+                name="password2"
+              />
+            </Form.Group>
+            <Button type="submit" className={btnStyling.Button}>
               Submit
             </Button>
           </Form>
         </Col>
-         <Col>
-         <Container>
-          <Link to="/signin">
-            Already have an account?
-             <span>Sign in</span>
+
+        <Col
+          xs={12}
+          md={6}
+          lg={6}
+          xl={6}
+          className={`d-none d-md-block ${formStyling.ImageCol}`}
+        >
+          <Image
+            src="https://res.cloudinary.com/dmod5eglu/image/upload/v1703462762/form_img_bl8th5.png"
+            className={formStyling.Image}
+          />
+        </Col>
+      </Row>
+
+      <Row className="p-2">
+        <Col
+          xs={12}
+          md={6}
+          lg={6}
+          xl={6}
+          className={`d-none d-md-block ${formStyling.ImageCol}`}
+        >
+          <Image
+            src="https://res.cloudinary.com/dmod5eglu/image/upload/v1703462762/form_img_bl8th5.png"
+            className={formStyling.Image}
+          />
+        </Col>
+        <Col
+          xs={12}
+          md={6}
+          lg={6}
+          xl={6}
+          className={`mb-4 mb-md-0 ${formStyling.Link} ${formStyling.Form}`}
+        >
+          <Link xs={12} md={6} lg={6} xl={6} to="/signin">
+            <p>
+              Already have an account? <span>Sign in</span>
+            </p>
           </Link>
-        </Container>
-         </Col>
-    
-        <Col className="text-center">
-        <Image
-          className={formStyling.Image}
-          src={
-            "https://res.cloudinary.com/dmod5eglu/image/upload/v1703462762/form_img_bl8th5.png"
-            
-          } 
-        />
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
