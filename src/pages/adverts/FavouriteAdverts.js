218339caecb7f2a7843dda746c435237154c6c42
AsiaWi <joannawitek@icloud.com>
2024-01-23 22:44:15 +0000

    Add inactive styling to favourite ads sidebar

--- src/pages/adverts/FavouriteAdverts.js
+++ src/pages/adverts/FavouriteAdverts.js
@@ -4,11 +4,12 @@ import Avatar from "../../components/Avatar";
 
 const FavouriteAdverts = (props) => {
   const { advert, mobile } = props;
-  const { id, image, price, advert_title} = advert;
+  const { id, image, price, advert_title, active} = advert;
 
   return (
     <div
       className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+      style={{ opacity : !active ? "40%" : null}}
     >
       <div>
         <Link className="align-self-center" to={`/adverts/${id}`}>
